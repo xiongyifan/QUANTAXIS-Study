2e92796c9d5cdc105d55ea7e3c38a102db87efd6
xiongyifan <xyf0728@126.com>
2022-02-21 19:08:01 +0800

    update

--- bash/install_qa.sh
+++ bash/install_qa.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # 安装QA
-# Step 1: 下载docker-compose.yaml文件
+# 1. 下载docker-compose.yaml文件
 # wget https://gitee.com/xiongyifan/quantaxis-study/raw/master/docker/docker-compose.yaml  # 不会覆盖已有文件
 wget https://gitee.com/xiongyifan/quantaxis-study/raw/master/docker/docker-compose.yaml -O docker-compose.yaml  # 会覆盖已有文件
-# Step 2: 创建数据卷
+# 2. 创建数据卷
 sudo docker volume create --name=qamg
 sudo docker volume create --name=qacode
 sudo docker volume create --name=pg-data
@@ -12,37 +12,69 @@ sudo docker volume create --name=qadag
 sudo docker volume create --name=qaconf
 sudo docker volume create --name=qauser
 sudo docker volume create --name=qalog
-# Step 3: 下载并创建docker容器
+# 3. 下载并创建docker容器
 sudo docker-compose up -d
-# Step 4: 安装新版pytdx，quantaxis
+# 4. 安装群文件中的pytdx，quantaxis
+# 4.1 获取pytdx，quantaxis的文件名
+fileNames=$(ls)
+qaFileName=''
+pytdxFileName=''
+
+for fileName in $fileNames
+do
+	if [ ${fileName:0:5} == 'pytdx' ]
+	then
+   		pytdxFileName=$fileName
+  fi
+
+  if [ "${fileName:0:9}" == 'quantaxis' ]
+	then
+   		qaFileName=$fileName
+  fi
+done
+
+echo ${pytdxFileName}
+echo ${qaFileName}
+
+# 4.2 安装函数
 installNewPytdxQA(){
-    # qacommunity-rust
-	find_str=$1
-	container=$2
+  # findStr，当容器日志中出现指定字符串时说明容器启动完成了，这个时候可以开始安装群文件中的qa和pytdx版本。
+  # 这么做的原因是容器自己也会安装qa和pytdx，要等它先装完，然后你再卸载安装，否则你装的会被容器安装的覆盖掉。
+	findStr=$1
+	# containerName, 容器名，这个容器名在docker-compose.yaml中container_name中指定
+	containerName=$2
+	# isInstallQA，是否安装群文件中的qa。qamarketcollector容器可以不安装群中qa版本，因为不影响使用。
 	isInstallQA=$3
-    pydtxFileName="pytdx-1.72r2-py3-none-any.whl"
-    quantaxisFileName="quantaxis-1.10.19r3-py3-none-any.whl"
-	echo ${container}
-	find_file=`sudo docker inspect --format='{{.LogPath}}' ${container}`
+	echo ${containerName}
+	# 找到容器对应的日志文件
+	findFile=$(sudo docker inspect --format='{{.LogPath}}' ${containerName})
+	echo ${findFile}
+	# 在日志文件中不断的查到findStr，找到后说明容器已经初始化完成，可以开始安装qa和pytdx。
 	while :
 	do
 		# 判断匹配函数，匹配函数不为0，则包含给定字符
-		if [ `sudo grep -c "${find_str}" ${find_file}` -ne '0' ];then
-			sudo docker cp ./${pydtxFileName} ${container}:/root
-			sudo docker exec ${container} pip uninstall pytdx -y
-			sudo docker exec ${container} pip install /root/${pydtxFileName}
+		if [ `sudo grep -c "${findStr}" ${findFile}` -ne '0' ];then
+		  # 安装pytdx
+			sudo docker cp ./${pydtxFileName} ${containerName}:/root
+			sudo docker exec ${containerName} pip uninstall pytdx -y
+			sudo docker exec ${containerName} pip install /root/${pydtxFileName}
 
+      # 安装qa
 			if [ ${isInstallQA} = "true" ];then
-				sudo docker cp ./${quantaxisFileName} ${container}:/root
-				sudo docker exec ${container} pip uninstall quantaxis -y
-				sudo docker exec ${container} pip install /root/${quantaxisFileName}
+				sudo docker cp ./${quantaxisFileName} ${containerName}:/root
+				sudo docker exec ${containerName} pip uninstall quantaxis -y
+				sudo docker exec ${containerName} pip install /root/${quantaxisFileName}
 			fi
 			break
 		fi
 		sleep 1s
 	done
 }
+# 4.3 安装
+# 以，使用control-c停止此服务器，作用findStr，是根据容器首次启动的日志提取出来的，
+# 当日志不再打印时，说明容器首次启动完成，在日志末尾找一段文本，测试能够在卸载后重新安装即可。
 installNewPytdxQA "使用control-c停止此服务器" "qacommunity-rust" "true"
+# mingle: all alone，同理
 installNewPytdxQA "mingle: all alone" "qaweb" "true"
 installNewPytdxQA "if you use ssh" "qamarketcollector" "false"
 
